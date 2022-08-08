8bfe4fd3cd25c5f3aab0659cf220c997a84490a6
Zitouni Lokmane <94765997+iAmLaZa@users.noreply.github.com>
2022-08-08 16:22:59 +0100

    LED RGB

--- color_detection.py
+++ color_detection.py
@@ -3,57 +3,54 @@ import Adafruit_TCS34725
 import RPi.GPIO as GPIO
 tcs = Adafruit_TCS34725.TCS34725()
 tcs.set_interrupt(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(32, GPIO.OUT)
-GPIO.setup(36, GPIO.OUT)
-GPIO.setup(38, GPIO.OUT)
-pr = GPIO.PWM(32,50)
-pg = GPIO.PWM(36,50)
-pb = GPIO.PWM(38,50)
-pr.start(20)
-pg.start(20)
-pb.start(20)
+
+GPIO.setmode(GPIO.BCM)
+RED = 25
+GREEN = 24
+BLUE = 23
+
+GPIO.setup(RED,GPIO.OUT)
+GPIO.output(RED,0)
+
+GPIO.setup(GREEN,GPIO.OUT)
+GPIO.output(GREEN,0)
+
+GPIO.setup(BLUE,GPIO.OUT)
+GPIO.output(BLUE,0)
+
+
 
 while True:
-    r, g, b, c = tcs.get_raw_data()
-    if((r > b) and (r >g)):
-        if(b <5):
-            #red
+    try :
+        r, g, b, c = tcs.get_raw_data()
+        if((r > b) and (r >g)):
+            if(b <5):
+                #red
+                #moteur.stop()
+                time.sleep(5)
+                #servo motor italien b degré t3 90 
+                #moteur.start()
+                
+                
+        elif((r < b) and (b >5)):
+            #blue
             #moteur.stop()
-            pr.ChangeDutyCycle(80)
-            pg.ChangeDutyCycle(1)
-            pb.ChangeDutyCycle(1)
-            time.sleep(5)
-            #servo motor italien b degré t3 90 
+            time.sleep(2)
             #moteur.start()
             
+        elif((r < g) and (b <g) and (g>20)):
+            if (b<10):
+                #green 
+                #moteur.stop()
+                time.sleep(5)
+                #moteur.start()
             
-    elif((r < b) and (b >5)):
-        #blue
-        #moteur.stop()
-        pr.ChangeDutyCycle(1)
-        pg.ChangeDutyCycle(1)
-        pb.ChangeDutyCycle(80)
-        time.sleep(2)
-        #moteur.start()
-        
-    elif((r < g) and (b <g) and (g>20)):
-        if (b<10):
-            #green 
-            #moteur.stop()
-            pr.ChangeDutyCycle(1)
-            pg.ChangeDutyCycle(80)
-            pb.ChangeDutyCycle(1)
-            time.sleep(5)
-            #moteur.start()
-    else:
-        pr.ChangeDutyCycle(20)
-        pg.ChangeDutyCycle(20)
-        pb.ChangeDutyCycle(20)
+        GPIO.output(RED,r)
+        GPIO.output(GREEN,g)
+        GPIO.output(BLUE,b)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 
 tcs.set_interrupt(True)
 tcs.disable()
-pr.stop()
-pg.stop()
-pb.stop()
