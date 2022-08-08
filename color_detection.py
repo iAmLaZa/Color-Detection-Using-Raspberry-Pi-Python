4231edccbfd469c36e7267362e5a20bf95ebe7cc
Zitouni Lokmane <94765997+iAmLaZa@users.noreply.github.com>
2022-08-08 16:31:36 +0100

    LED_RGB

--- color_detection.py
+++ color_detection.py
@@ -10,46 +10,53 @@ GREEN = 24
 BLUE = 23
 
 GPIO.setup(RED,GPIO.OUT)
-GPIO.output(RED,0)
+GPIO.output(RED,255)
 
 GPIO.setup(GREEN,GPIO.OUT)
-GPIO.output(GREEN,0)
+GPIO.output(GREEN,255)
 
 GPIO.setup(BLUE,GPIO.OUT)
-GPIO.output(BLUE,0)
+GPIO.output(BLUE,255)
 
 
 
 while True:
-    try :
-        r, g, b, c = tcs.get_raw_data()
-        if((r > b) and (r >g)):
-            if(b <5):
-                #red
-                #moteur.stop()
-                time.sleep(5)
-                #servo motor italien b degré t3 90 
-                #moteur.start()
-                
-                
-        elif((r < b) and (b >5)):
-            #blue
+    r, g, b, c = tcs.get_raw_data()
+    if((r > b) and (r >g)):
+        if(b <5):
+            #red
             #moteur.stop()
-            time.sleep(2)
+            GPIO.output(RED,80)
+            GPIO.output(GREEN,1)
+            GPIO.output(BLUE,1)
+            time.sleep(5)
+            #servo motor italien b degré t3 90 
             #moteur.start()
             
-        elif((r < g) and (b <g) and (g>20)):
-            if (b<10):
-                #green 
-                #moteur.stop()
-                time.sleep(5)
-                #moteur.start()
             
-        GPIO.output(RED,r)
-        GPIO.output(GREEN,g)
-        GPIO.output(BLUE,b)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+    elif((r < b) and (b >5)):
+        #blue
+        #moteur.stop()
+        GPIO.output(RED,1)
+        GPIO.output(GREEN,1)
+        GPIO.output(BLUE,80)
+        time.sleep(2)
+        #moteur.start()
+        
+    elif((r < g) and (b <g) and (g>20)):
+        if (b<10):
+            #green 
+            #moteur.stop()
+            GPIO.output(RED,1)
+            GPIO.output(GREEN,80)
+            GPIO.output(BLUE,1)
+            time.sleep(5)
+            #moteur.start()
+    else:
+        #default color
+        GPIO.output(RED,255)
+        GPIO.output(GREEN,255)
+        GPIO.output(BLUE,255)
 
 
 tcs.set_interrupt(True)
